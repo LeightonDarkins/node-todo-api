2cafedfc6a949bdd7f9ffa3cda7ac08afe04f807
Leighton Darkins <ldarkins@thoughtworks.com>
2016-11-29 21:33:42 +1000

    LD - hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (error, salt) => {
+  bcrypt.hash(password, salt, (error, hash) => {
+    console.log(hash);
+  });
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$2qx0TVM6cAvvg.7MQEQ9j.qETIPEyWnuYcHPyqk/S/MN0bUBGgAR.';
+
+bcrypt.compare(password, hashedPassword, (error, result) => {
+  console.log(result);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
